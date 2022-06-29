e2daccc4575b02dbf9b30d32ccb1184cda73920d
Eduardo Arndt <eduardoarndt1@gmail.com>
2022-06-28 21:39:43 -0300

    refactor stack

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -6,14 +6,6 @@ import {
 } from "@serverless-stack/resources";
 
 export function MyStack({ stack }: StackContext) {
-  // Create the table
-  const table = new Table(stack, "Counter", {
-    fields: {
-      counter: "string",
-    },
-    primaryIndex: { partitionKey: "counter" },
-  });
-
   const resumeTable = new Table(stack, "resume-table", {
     fields: {
       name: "string",
@@ -23,21 +15,6 @@ export function MyStack({ stack }: StackContext) {
       experience: "string",
     },
     primaryIndex: { partitionKey: "email" },
-  });
-
-  // Create the HTTP API
-  const api = new Api(stack, "Api", {
-    defaults: {
-      function: {
-        // Pass in the table name to our API
-        environment: {
-          tableName: table.tableName,
-        },
-      },
-    },
-    routes: {
-      "POST /": "functions/lambda.main",
-    },
   });
 
   const resumeApi = new Api(stack, "resume-api", {
@@ -57,24 +34,17 @@ export function MyStack({ stack }: StackContext) {
     },
   });
 
-  // Allow the API to access the table
-  api.attachPermissions([table]);
-
   resumeApi.attachPermissions([resumeTable]);
 
-  // Deploy our React app
   const site = new ReactStaticSite(stack, "ReactSite", {
     path: "frontend",
     environment: {
-      REACT_APP_API_URL: api.url,
       REACT_APP_RESUME_API_URL: resumeApi.url,
     },
   });
 
-  // Show the URLs in the output
   stack.addOutputs({
     SiteUrl: site.url,
-    ApiEndpoint: api.url,
     ResumeApiEndpoint: resumeApi.url,
   });
 }
